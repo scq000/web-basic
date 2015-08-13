0b6769fe6ff7c816d9220d08317098031531571a
scq000 <scq000@hotmail.com>
2015-08-13 15:19:23 +0800

    add HomeViewModel Class

---
+++ view-models/home-view-model.js
@@ -0,0 +1,37 @@
+'use strict';
+
+function HomeViewModel(topics) {
+    this.topics;
+}
+
+HomeViewModel.prototype.getFillInBlankTopic = function () {
+    return this.topics.filter(function(topic) {
+        return topic.type === 'fill_in_blank';
+    });
+};
+
+HomeViewModel.prototype.getSingleChoiceTopic = function () {
+    return this.topics.filter(function(topic) {
+        return topic.type === 'single_choice';
+    });
+};
+
+HomeViewModel.prototype.getMultipleChoiceTopic = function () {
+    return this.topics.filter(function(topic) {
+        return topic.type === 'multiple_choice';
+    });
+};
+
+HomeViewModel.prototype.getJudgeTopic = function () {
+    return this.topics.filter(function(topic) {
+        return topic.type === 'judge';
+    });
+};
+
+HomeViewModel.prototype.getSimpleAnswerTopic = function () {
+    return this.topics.filter(function(topic) {
+        return topic.type === 'simple_answer';
+    });
+};
+
+module.exports = HomeViewModel;
