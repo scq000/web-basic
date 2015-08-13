a3ef3383653a1273c8b49f0ba84ba5b5abf45b64
scq000 <scq000@hotmail.com>
2015-08-13 15:04:00 +0800

    add TopicHelper Class

---
+++ helper/topic-helper.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var TopicFactory = require('../model/topic-factory');
+
+function TopicHelper() {
+}
+
+TopicHelper.prototype.getTopics = function (data) {
+    var topics = [];
+
+    var topicFactory = new TopicFactory();
+    for(var type in data) {
+        data[type].forEach(function(topicData) {
+            topics.push(topicFactory.create(type,topicData));
+        });
+    }
+
+    return topics;
+};
+
+
+module.exports = TopicHelper;
