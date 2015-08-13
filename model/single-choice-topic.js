8068a96102ea91e5a8fab0821f77e6336a19467c
scq000 <scq000@hotmail.com>
2015-08-13 14:43:19 +0800

    add SingleChoiceTopic class

---
+++ model/single-choice-topic.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var Topic = require('./topic');
+
+function SingleChoiceTopic(type,name,question,options,score,answer){
+    Topic.call(this,"fill_in_blank",name,question,options,score,answer);
+}
+
+SingleChoiceTopic.prototype = Object.create(Topic.prototype);
+SingleChoiceTopic.prototype.constructor = SingleChoiceTopic;
+
+SingleChoiceTopic.prototype.mark = function(){};
+
+module.exports = SingleChoiceTopic;
