bf557d2d140e42a1fabb285059c6ea0cf5da66ea
scq000 <scq000@hotmail.com>
2015-08-13 14:56:54 +0800

    add TopicFactory class

---
+++ model/topic-factory.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var Topic = require('./topic');
+var FillInBlankTopic = require('./fill-in-blank-topic');
+var SingleChoiceTopic = require('./single-choice-topic');
+var MultipleChoiceTopic = require('./multiple-choice-topic');
+var JudgeTopic = require('./judge-topic');
+var SimpleAnswerTopic = require('./simple-answer-topic');
+
+function TopicFactory(){}
+
+TopicFactory.prototype.create = function(type,data){
+    if(type === 'fill_in_blank') {
+        return new FillInBlankTopic(data.name,data.question,data.options,data.score,data.answer);
+    }else if(type === 'single_choice') {
+        return new SingleChoiceTopic(data.name,data.question,data.options,data.score,data.answer);
+    }else if(type === 'multiple_choice') {
+        return new MultipleChoiceTopic(data.name,data.question,data.options,data.score,data.answer);
+    }else if(type === 'judge') {
+        return new JudgeTopic(data.name,data.question,data.options,data.score,data.answer);
+    }else if(type === 'simple_answer') {
+        return new SimpleAnswerTopic(data.name,data.question,data.options,data.score,data.answer);
+    }
+};
+
+module.exports = TopicFactory;
