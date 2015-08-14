69c23dca6e52c62e704767174f9ec613ab655d5f
scq000 <scq000@hotmail.com>
2015-08-14 21:03:10 +0800

    use app.js not bin/www

--- app.js
+++ app.js
@@ -17,4 +17,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 route.setRoutes(app);
 
+app.listen(3000, function () {
+    console.log('You are running on port 3000');
+});
+
 module.exports = app;
