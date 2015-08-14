6e277c6a3dc2d7d09cf96bc23e8b3040de9e62c9
scq000 <scq000@hotmail.com>
2015-08-15 07:01:15 +0800

    merge judgeUserInput to main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,16 @@
 $(document).ready(function () {
     $("#submit_btn").on('click', function () {
-        if (judgeUserInput()) {
+        var validity = true;
+        $('.student_info').each(function () {
+            if ($(this).val() === '') {
+                validity = false;
+                $(this).css("border", "1px solid red");
+            } else {
+                $(this).css("border", "1px solid #ccc");
+            }
+        });
+
+        if (validity) {
             $("form").submit();
         } else {
             $("#alert_info").modal("show");
