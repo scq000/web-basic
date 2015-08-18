fa0226e34e277f613689f8eef1163508295a78df
scq000 <scq000@hotmail.com>
2015-08-18 14:35:17 +0800

    init database by gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require('gulp');
+var execsql = require('execsql');
+var dbConfig = require('./config/database');
 
 gulp.task('init', function() {
-    
+  var connection = execsql.config(dbConfig);
+
+  connection.execFile('./seeds/topics.sql', function(err, results) {
+    if (err) {
+      console.log(err);
+    }
+
+    console.log(results);
+
+    connection.end();
+  });
+
 });
