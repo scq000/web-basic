2d6a57ff10139445f49204c8e0c97d6fb8f048ee
scq000 <scq000@hotmail.com>
2015-08-18 15:49:21 +0800

    create tables

--- seeds/topics.sql
+++ seeds/topics.sql
@@ -1,3 +1,22 @@
 DROP DATABASE IF EXISTS TWWebBasic;
 
 CREATE DATABASE TWWebBasic;
+
+USE TWWebBasic;
+
+CREATE TABLE  IF NOT EXISTS alternative(
+    id int NOT NULL PRIMARY KEY ,
+    value varchar(10) ,
+    description varchar(50) ,
+    topic_name varchar(20) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS topic(
+  name VARCHAR(20) NOT NULL PRIMARY KEY,
+  type VARCHAR(20) NOT NULL,
+  question VARCHAR(50) NOT NULL,
+  score INT NOT NULL
+);
+
+ALTER TABLE alternative ADD CONSTRAINT alternative_topic FOREIGN KEY alternative_topic (topic_name)
+    REFERENCES topic (name)  ON DELETE CASCADE ON UPDATE CASCADE;
